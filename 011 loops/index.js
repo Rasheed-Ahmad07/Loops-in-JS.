521fbe39fcd8fbeb107855ab3cf78bfa6cbf6692
Rasheed Ahmad <118961857+Rasheed-Ahmad07@users.noreply.github.com>
2024-01-25 23:18:05 +0500

    Add files via upload

---
+++ 011 loops/index.js
@@ -0,0 +1,21 @@
+// for loop
+for (i = 300; i >= 0; i--) {
+  console.log(i);
+}
+
+
+
+// while
+let j = 50;
+while (j >= 40) {
+  console.log(i);
+  j--;
+}
+
+
+// Do while
+let k = 0;
+do {
+  console.log(k);
+  i++;
+} while (k < 5);
